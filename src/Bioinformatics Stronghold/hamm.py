4d349cd259dfeb0331c3673b981d865658f74452
Electrocyte96 <jesusjacobgm@gmail.com>
2026-03-20 13:41:00 -0600

    New solved exercise

--- src/Bioinformatics Stronghold/hamm.py
+++ src/Bioinformatics Stronghold/hamm.py
@@ -4,11 +4,18 @@ Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
 
 Return: The Hamming distance dH(s,t).
 '''
-def hamming(s:str, )
+def hamming(s:str, t:str):
+    count = 0
+    if len(s) == len(t):
+        for nt in range(len(s)):
+            if s[nt] != t[nt]:
+                count+=1
+    return count
 
 def main():
     s='GAGCCTACTAACGGGAT'
     t='CATCGTAATGACGGCCT'
+    print(hamming(s,t))
 
 if __name__ == "__main__":
     main()
