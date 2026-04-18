047711eedb6e34518c6a31530b1256489d169cf8
Electrocyte96 <jesusjacobgm@gmail.com>
2026-04-18 12:58:28 -0600

    New solved exercise

--- src/Bioinformatics Stronghold/orf.py
+++ src/Bioinformatics Stronghold/orf.py
@@ -9,22 +9,6 @@ dna_seqs = '''
 >Rosalind_99
 AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG
 '''.splitlines()
-codons = {'UUU': 'F',      'CUU': 'L',      'AUU': 'I',      'GUU': 'V',
-    'UUC': 'F',      'CUC': 'L',      'AUC': 'I',      'GUC': 'V',
-    'UUA': 'L',      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
-    'UUG': 'L',      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
-    'UCU': 'S',      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
-    'UCC': 'S',      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
-    'UCA': 'S',      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
-    'UCG': 'S',      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
-    'UAU': 'Y',      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
-    'UAC': 'Y',      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
-    'UAA': 'Stop',   'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
-    'UAG': 'Stop',   'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
-    'UGU': 'C',      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
-    'UGC': 'C',      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
-    'UGA': 'Stop',   'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
-    'UGG': 'W',      'CGG': 'R',      'AGG': 'R',      'GGG': 'G'} 
 
 def fasta_to_dict(dna_strings:str)->dict:
     seq_dict = {}
@@ -52,47 +36,62 @@ def complement(s:str)->str:
         s_complement += complement[nt]
     return s_complement
 
-def reading_frame(dna_seqs:str)->set:
-    dna_dict = fasta_to_dict(dna_seqs)
-    total_prots = set()
+def rna_to_prot(s:str)->str:
+    codons = {'UUU': 'F',      'CUU': 'L',      'AUU': 'I',      'GUU': 'V',
+    'UUC': 'F',      'CUC': 'L',      'AUC': 'I',      'GUC': 'V',
+    'UUA': 'L',      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
+    'UUG': 'L',      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
+    'UCU': 'S',      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
+    'UCC': 'S',      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
+    'UCA': 'S',      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
+    'UCG': 'S',      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
+    'UAU': 'Y',      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
+    'UAC': 'Y',      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
+    'UAA': 'Stop',   'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
+    'UAG': 'Stop',   'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
+    'UGU': 'C',      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
+    'UGC': 'C',      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
+    'UGA': 'Stop',   'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
+    'UGG': 'W',      'CGG': 'R',      'AGG': 'R',      'GGG': 'G'} 
+    n = len(s)
+    k = 3
+    protein = []
+    for i in range(0, n, k):
+        amino = s[i:i+3]
+        if len(amino) == 3:
+            if codons[amino] == 'Stop':
+                break
+            protein.append(codons[amino])
+    return ''.join(protein)
+
+def reading_frame(dna_dict:dict)->set:
+    proteins = set()
     for key, value in dna_dict.items():
-        rna = to_rna(value)
         dna_c = complement(value)
         rna_c = to_rna(dna_c)
-        k, n = 3, len(rna)
+        rna = to_rna(value)
+        n,k = len(rna),3
         for i in range(n-k+1):
-            met = rna[i:i+k]
-            prot = []
-            if met == 'AUG':
-                for j in range(i,n,k):
-                    cod = rna[j:j+k]
-                    if len(cod) == 3: 
-                        if codons[cod] == 'Stop':
-                            break
-                        else:
-                            prot.append(codons[cod])
-                prot = ''.join(prot)
-                total_prots.add(prot)
-        
+            cod = rna[i:i+k]
+            if len(cod) == 3:
+                if cod == 'AUG':
+                    prot = rna_to_prot(rna[i:])
+                    if len(prot)*3 <= n-i-k:
+                        proteins.add(prot)
         for i in range(n-k+1):
-            met = rna_c[i:i+k]
-            prot = []
-            if met == 'AUG':
-                for j in range(i,n,k):
-                    cod = rna_c[j:j+k]
-                    if len(cod) == 3: 
-                        if codons[cod] == 'Stop':
-                            break
-                        else:
-                            prot.append(codons[cod])
-                prot = ''.join(prot)
-                total_prots.add(prot)
-                
-    return total_prots
+            cod = rna_c[i:i+k]
+            if len(cod) == 3:
+                if cod == 'AUG':
+                    prot = rna_to_prot(rna_c[i:])
+                    if len(prot)*3 <= n-i-k:
+                        proteins.add(prot)
+        return proteins
 
 def main():
-    print(reading_frame(dna_seqs))
-
+    dna_dict = fasta_to_dict(dna_seqs)
+    proteins = reading_frame(dna_dict)
+    for prot in proteins:
+        print(prot)
 
 if __name__ == "__main__":
     main()
