ea9d692d9e733617cd99cc0aa41c269a900ae489
Electrocyte96 <jesusjacobgm@gmail.com>
2026-05-29 23:03:26 -0600

    Added new proplem file

---
+++ src/Bioinformatics Stronghold/prob.py
@@ -0,0 +1,14 @@
+#   Introduction to Random Strings
+'''
+Given: A DNA string s of length at most 100 bp and an array 
+A containing at most 20 numbers between 0 and 1.
+
+Return: An array B having the same length as A in which B[k] 
+represents the common logarithm of the probability that a random
+string constructed with the GC-content found in A[k] will match s exactly.
+'''
+def main():
+    
+
+if __name__ == "__main__":
+    main()
