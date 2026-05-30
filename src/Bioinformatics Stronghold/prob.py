3f479427f3c4dd31564d8186b03b2a51d6db8785
Electrocyte96 <jesusjacobgm@gmail.com>
2026-05-29 23:57:00 -0600

    Solved new exercise

--- src/Bioinformatics Stronghold/prob.py
+++ src/Bioinformatics Stronghold/prob.py
@@ -7,8 +7,30 @@ Return: An array B having the same length as A in which B[k]
 represents the common logarithm of the probability that a random
 string constructed with the GC-content found in A[k] will match s exactly.
 '''
+import math
+
+def gc_prob(a:str, b_num:list)->list:
+    b = [float(x) for x in b_num]
+    n = len(a)
+    p_totals = []
+    for b_x in b:
+        result = 0.0
+        p_gc = b_x/2
+        p_at = (1-b_x)/2
+        for i in range(n):
+            if a[i] == 'G' or a[i] == 'C':
+                result += math.log10(p_gc)
+            else:
+                result += math.log10(p_at)
+        p_totals.append(round(result, 3))
+    return p_totals
+
 def main():
-    
+    a = 'ACGATACAA'
+    b_num = '0.129 0.287 0.423 0.476 0.641 0.742 0.783'.split()
+    p_results = gc_prob(a, b_num)
+    for p in p_results:
+        print(p, end=' ')
 
 if __name__ == "__main__":
     main()
