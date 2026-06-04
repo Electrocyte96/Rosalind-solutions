2b123fc2498c77fd11f0247ce0046828f7e5b1a5
Electrocyte96 <jesusjacobgm@gmail.com>
2026-06-04 15:50:15 -0600

    Added a more 'pythonic' function

--- src/Bioinformatics Stronghold/tran.py
+++ src/Bioinformatics Stronghold/tran.py
@@ -20,7 +20,7 @@ def fasta_to_dict(dna_seqs:str)->dict:
         seq_dict[header] = ''.join(seq)
     return seq_dict
 
-def traversion_transition_ratio(s:str, t:str):
+def traversion_transition_ratio(s:str, t:str)->float:
     transition, traversion = 0,0
     if len(s) == len(t):
         for nt in range(len(s)):
@@ -38,6 +38,18 @@ def traversion_transition_ratio(s:str, t:str):
                 traversion += 1
     return transition/traversion
 
+def traversion_transition_ratio_2(s:str, t:str)->float:
+    transitions,  trans= ('AG', 'GA', 'CT', 'TC'), 0
+    traversions, trav = ('AC', 'AT', 'CA', 'CG', 'TA', 'TG', 'GC', 'GT'), 0
+    if len(s)==len(t):
+        n = len(s)
+        for i in range(n):
+            if s[i]+t[i] in transitions:
+                trans += 1
+            elif s[i]+t[i] in traversions:
+                trav += 1
+    return trans/trav
+
 def main():
     dna = '''
 >Rosalind_0209
@@ -51,6 +63,8 @@ GGTACGAGTGTTCCTTTGGGT
     seqs = list(dna_dict.values())
     ratio = traversion_transition_ratio(seqs[0], seqs[1])
     print(f'{ratio:.11f}')
+    ratio_2 = traversion_transition_ratio_2(seqs[0], seqs[1])
+    print(f'{ratio_2:.11f}')
 
 if __name__ == "__main__":
     main()
