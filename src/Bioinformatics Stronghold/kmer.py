6ec3e619d45b58948a85be375114febf033094a8
Electrocyte96 <jesusjacobgm@gmail.com>
2026-06-13 23:45:14 -0600

    Created file and sort of template

---
+++ src/Bioinformatics Stronghold/kmer.py
@@ -0,0 +1,36 @@
+#   k-Mer Composition
+'''
+Given: A DNA string s in FASTA format (having length at most 100 kbp).
+
+Return: The 4-mer composition of s.
+'''
+def fasta_to_dict(dna_strings:str)->dict:
+    seq_dict = {}
+    seq=[]
+    header = None
+    for i in range(len(dna_strings)):
+        if dna_strings[i].startswith('>'):
+            if header:
+                seq_dict[header] = ''.join(seq)
+            header = dna_strings[i][1:]
+            seq = []
+        else:
+            seq.append(dna_strings[i])
+    if header:
+        seq_dict[header] = ''.join(seq)
+    return seq_dict
+
+def main():
+    dna_fas = '''>Rosalind_6431
+CTTCGAAAGTTTGGGCCGAGTCTTACAGTCGGTCTTGAAGCAAAGTAACGAACTCCACGG
+CCCTGACTACCGAACCAGTTGTGAGTACTCAACTGGGTGAGAGTGCAGTCCCTATTGAGT
+TTCCGAGACTCACCGGGATTTTCGATCCAGCCTCAGTCCAGTCTTGTGGCCAACTCACCA
+AATGACGTTGGAATATCCCTGTCTAGCTCACGCAGTACTTAGTAAGAGGTCGCTGCAGCG
+GGGCAAGGAGATCGGAAAATGTGCTCTATATGCGACTAAAGCTCCTAACTTACACGTAGA
+CTTGCCCGTGTTAAAAACTCGGCTCACATGCTGTCTGCGGCTGGCTGTATACAGTATCTA
+CCTAATACCCTTCAGTTCGCCGCACAAAAGCTGGGAGTTACCGCGGAAATCACAG
+'''.splitlines()
+    
+
+if __name__ == "__main__":
+    main()
